618eed2be5c33cbea645835aa74e87a1dd8e23d0
Norbert00 <norbert.jurek@protonmail.com>
2023-05-25 10:17:55 +0200

    Added resource aws_api_gateway_integration, dependencies to resources aws_api_gateway_stage, aws_api_gateway_deployment to correctly build API Gateway  resource

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 #*  dynamodb table
-resource "aws_dynamodb_table" "dynamodb-table" {
+resource "aws_dynamodb_table" "dynamodb_table" {
   name           = "books"
   billing_mode   = "PROVISIONED"
   read_capacity  = 1
@@ -22,7 +22,7 @@ resource "aws_dynamodb_table" "dynamodb-table" {
   }
 }
 
-#*  api gateway
+# #*  api gateway
 resource "aws_api_gateway_rest_api" "books_api" {
   api_key_source = "HEADER"
 
@@ -32,7 +32,7 @@ resource "aws_api_gateway_rest_api" "books_api" {
   put_rest_api_mode            = "overwrite"
 
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy = false
   }
 
 
@@ -43,38 +43,54 @@ resource "aws_api_gateway_rest_api" "books_api" {
   }
 }
 
-
-resource "aws_api_gateway_deployment" "api_deployment" {
+resource "aws_api_gateway_resource" "books_path" {
   rest_api_id = aws_api_gateway_rest_api.books_api.id
+  parent_id   = aws_api_gateway_rest_api.books_api.root_resource_id
+  path_part   = "books"
+}
 
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.books_api.body))
-  }
+resource "aws_api_gateway_method" "api_methods" {
+  api_key_required = false
+  authorization    = "NONE"
+  http_method      = "GET"
+  resource_id      = aws_api_gateway_resource.books_path.id
+  rest_api_id      = aws_api_gateway_rest_api.books_api.id
 
-  lifecycle {
-    create_before_destroy = true
-  }
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.books_api.id
+  resource_id             = aws_api_gateway_resource.books_path.id
+  http_method             = aws_api_gateway_method.api_methods.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
 
 resource "aws_api_gateway_stage" "api_stage" {
   cache_cluster_enabled = false
-  deployment_id         = "zefn9o"
-  rest_api_id           = "ixgf79heae"
+  deployment_id         = aws_api_gateway_deployment.api_deployment.id
+  rest_api_id           = aws_api_gateway_rest_api.books_api.id
   stage_name            = "dev"
   xray_tracing_enabled  = false
+
+  depends_on = [aws_api_gateway_deployment.api_deployment]
+
 }
 
-resource "aws_api_gateway_method" "api_method" {
-  api_key_required = false
-  authorization    = "NONE"
-  http_method      = "GET"
-  resource_id      = "ymmokh"
-  rest_api_id      = "ixgf79heae"
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.books_api.id
+
+  depends_on = [aws_api_gateway_method.api_methods, aws_api_gateway_integration.integration]
+
+  lifecycle {
+    create_before_destroy = false
+  }
 }
 
 
-#* policy 
+#*  policy 
 data "aws_iam_policy_document" "policy" {
   statement {
     sid = "VisualEditor0"
@@ -111,14 +127,16 @@ resource "aws_iam_policy" "lambda_policy" {
   policy = data.aws_iam_policy_document.policy.json
 }
 
-#* policy attachment
+#*  policy attachment
 resource "aws_iam_role_policy_attachment" "policy_attach" {
   policy_arn = "arn:aws:iam::109028672636:policy/dynamodb_crud"
   role       = "api-dynamodb"
+
+  depends_on = [aws_iam_role.iam_role]
 }
 
-#* role 
-resource "aws_iam_role" "tf_iam_role" {
+#*  role 
+resource "aws_iam_role" "iam_role" {
   assume_role_policy = jsonencode(
     {
       Statement = [
@@ -143,18 +161,20 @@ resource "aws_iam_role" "tf_iam_role" {
 }
 
 
-#* lambda
+#*  lambda
 resource "aws_lambda_function" "lambda" {
   function_name = "api-lambda"
-  handler       = "lambda_function.lambda_handler"
-  memory_size = 128
-  role     = "arn:aws:iam::109028672636:role/api-dynamodby"
-  runtime  = "python3.9"
-  filename = data.archive_file.lambda.output_path
-  
+  handler       = "api-lambda.lambda_handler"
+  memory_size   = 128
+  role          = "arn:aws:iam::109028672636:role/api-dynamodb"
+  runtime       = "python3.9"
+  filename      = data.archive_file.lambda.output_path
+
+
   lifecycle {
     ignore_changes = [filename, ]
   }
+}
 
 data "archive_file" "lambda" {
   type        = "zip"
@@ -167,6 +187,6 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   action        = "lambda:InvokeFunction"
   function_name = "arn:aws:lambda:eu-central-1:109028672636:function:api-lambda"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:eu-central-1:109028672636:ixgf79heae/*/GET/books"
+  source_arn    = "${aws_api_gateway_rest_api.books_api.execution_arn}/*/GET/books"
 }
 
